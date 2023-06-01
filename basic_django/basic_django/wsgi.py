049a2018347006a4d400d88a7872395b739c6247
Kishi <m.cankisi@gmail.com>
2023-06-01 10:06:41 +0100

    initial django and docker

--- basic_django/basic_django/wsgi.py
+++ basic_django/basic_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for help_c project.
+WSGI config for basic_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "help_c.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basic_django.settings")
 
 application = get_wsgi_application()
