77c6dd2cffd336bebf1740f9f4ef54e53a8e223d
Kishi <m.cankisi@gmail.com>
2023-06-23 08:58:05 +0100

    WIP wsgi

--- help_center/help_center/settings/base.py
+++ help_center/help_center/settings/base.py
@@ -25,8 +25,8 @@ SECRET_KEY = "django-insecure-^y258d-3872_ue3#giia$m*!2%!uu*_z%0!shd98=8v!eh*qdw
 # Application definition
 
 INSTALLED_APPS = [
-    "help_center.home",
-    "help_center.search",
+    "home",
+    "search",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.embeds",
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "help_center.blog",
+    "blog",
     "markdown",
 ]
 
