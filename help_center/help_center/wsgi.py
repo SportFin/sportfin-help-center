4bad0f05b35b986d3c83d4a78726bb9a22ac345b
Kishi <m.cankisi@gmail.com>
2023-06-23 08:36:22 +0100

    fix ref to settings

--- help_center/help_center/wsgi.py
+++ help_center/help_center/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "help_center.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "help_center.help_center.settings.dev")
 
 application = get_wsgi_application()
